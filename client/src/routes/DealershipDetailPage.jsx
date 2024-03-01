dab098899a2f860365528adf76bcb745f66689f0
Antkilla <132300802+Antkilla@users.noreply.github.com>
2024-02-29 21:58:18 -0800

    installed react router dom
    add all import statement

---
+++ client/src/routes/DealershipDetailPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DealershipDetailPage = () => {
+  return (
+    <div>
+        DealershipDetailPage
+    </div>
+  );
+};
+
+export default DealershipDetailPage
+
+
